3569a31b9305f7e104bc1387d03dfa6df21320eb
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-01 16:46:20 -0300

    Add PrimeVue pass through defaults

---
+++ src/components/shared/primePassThrough.js
@@ -0,0 +1,41 @@
+export const fmPrimePassThrough = {
+  button: {
+    root: { class: 'fm-pt-button' },
+    label: { class: 'fm-pt-button-label' },
+    icon: { class: 'fm-pt-button-icon' }
+  },
+  select: {
+    root: { class: 'fm-pt-select' },
+    label: { class: 'fm-pt-select-label' },
+    dropdown: { class: 'fm-pt-select-dropdown' },
+    panel: { class: 'fm-pt-select-panel' },
+    option: { class: 'fm-pt-select-option' }
+  },
+  multiselect: {
+    root: { class: 'fm-pt-multiselect' },
+    labelContainer: { class: 'fm-pt-multiselect-label' },
+    panel: { class: 'fm-pt-multiselect-panel' },
+    option: { class: 'fm-pt-multiselect-option' },
+    header: { class: 'fm-pt-multiselect-header' }
+  },
+  datatable: {
+    root: { class: 'fm-pt-datatable' },
+    table: { class: 'fm-pt-datatable-table' },
+    header: { class: 'fm-pt-datatable-header' },
+    footer: { class: 'fm-pt-datatable-footer' },
+    columnHeaderContent: { class: 'fm-pt-column-header' }
+  },
+  checkbox: {
+    root: { class: 'fm-pt-checkbox' },
+    box: { class: 'fm-pt-checkbox-box' },
+    icon: { class: 'fm-pt-checkbox-icon' }
+  },
+  dialog: {
+    root: { class: 'fm-pt-dialog' },
+    header: { class: 'fm-pt-dialog-header' },
+    content: { class: 'fm-pt-dialog-content' },
+    footer: { class: 'fm-pt-dialog-footer' }
+  }
+}
+
+export default fmPrimePassThrough
