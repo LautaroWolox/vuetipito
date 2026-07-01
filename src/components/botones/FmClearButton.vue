eef99537df6bd8b52297ece140f0c305a3b7fdf6
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-01 16:45:06 -0300

    Add FM clear button

---
+++ src/components/botones/FmClearButton.vue
@@ -0,0 +1,9 @@
+<template>
+  <button class="fm-btn fm-btn--outline" type="button" :disabled="disabled" @click="$emit('click')">
+    <span>{{ label }}</span>
+  </button>
+</template>
+<script setup>
+defineProps({ label: { type: String, default: 'LIMPIAR' }, disabled: { type: Boolean, default: false } })
+defineEmits(['click'])
+</script>
