d71c3ee6295347d6e2f9fdedc66e669ba23dc619
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-01 16:44:15 -0300

    Add global search button

---
+++ src/components/botones/BotonBuscar.vue
@@ -0,0 +1,10 @@
+<template>
+  <button class="fm-btn fm-btn--primary" type="button" :disabled="disabled" @click="$emit('click')">
+    <span class="material-icons fm-btn__icon">search</span>
+    <span>{{ label }}</span>
+  </button>
+</template>
+<script setup>
+defineProps({ label: { type: String, default: 'BUSCAR' }, disabled: { type: Boolean, default: false } })
+defineEmits(['click'])
+</script>
