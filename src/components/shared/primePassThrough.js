a3348786daab832ece782e65eea23bcbab4b723d
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-02 16:02:27 -0300

    Expand global PrimeVue pass through classes

--- src/components/shared/primePassThrough.js
+++ src/components/shared/primePassThrough.js
@@ -1,6 +1,6 @@
 export const fmPrimePassThrough = {
   button: {
-    root: { class: 'fm-pt-button' },
+    root: { class: 'fm-pt-button fm-action-button' },
     label: { class: 'fm-pt-button-label' },
     icon: { class: 'fm-pt-button-icon' }
   },
@@ -9,21 +9,29 @@ export const fmPrimePassThrough = {
     label: { class: 'fm-pt-select-label' },
     dropdown: { class: 'fm-pt-select-dropdown' },
     panel: { class: 'fm-pt-select-panel' },
+    list: { class: 'fm-pt-select-list' },
     option: { class: 'fm-pt-select-option' }
   },
   multiselect: {
     root: { class: 'fm-pt-multiselect' },
     labelContainer: { class: 'fm-pt-multiselect-label' },
     panel: { class: 'fm-pt-multiselect-panel' },
+    list: { class: 'fm-pt-multiselect-list' },
     option: { class: 'fm-pt-multiselect-option' },
     header: { class: 'fm-pt-multiselect-header' }
   },
   datatable: {
-    root: { class: 'fm-pt-datatable' },
+    root: { class: 'fm-pt-datatable fm-pass-grid' },
     table: { class: 'fm-pt-datatable-table' },
     header: { class: 'fm-pt-datatable-header' },
     footer: { class: 'fm-pt-datatable-footer' },
-    columnHeaderContent: { class: 'fm-pt-column-header' }
+    wrapper: { class: 'fm-pt-datatable-wrapper' },
+    thead: { class: 'fm-pt-datatable-thead' },
+    tbody: { class: 'fm-pt-datatable-tbody' },
+    row: { class: 'fm-pt-datatable-row' },
+    bodyRow: { class: 'fm-pt-datatable-body-row' },
+    columnHeaderContent: { class: 'fm-pt-column-header' },
+    paginator: { class: 'fm-pt-paginator' }
   },
   checkbox: {
     root: { class: 'fm-pt-checkbox' },
