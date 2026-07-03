a7684710219b15463219d45ba42a516e7f105532
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-03 13:38:12 -0300

    Refactor Nro OT element as PrimeVue input wrapper

--- src/modules/otFallidasCT/components/elementos/NroOT.vue
+++ src/modules/otFallidasCT/components/elementos/NroOT.vue
@@ -1,20 +1,33 @@
 <template>
-    <div class="flex flex-column px-2">
-        <label for="ot">Nro OT</label>
-        <InputText type="text" id="ot" v-model="nroOt"/>
-    </div>
+  <div class="fm-field fm-field--span-4 otf-filter-element otf-filter-element--nro-ot">
+    <label for="otf-nro-ot">Nro. OT</label>
+    <InputText
+      id="otf-nro-ot"
+      :modelValue="modelValue"
+      class="w-full"
+      type="text"
+      autocomplete="off"
+      @update:modelValue="$emit('update:modelValue', $event)"
+    />
+  </div>
 </template>
 
-
 <script setup>
-import { ref } from 'vue';
-import InputText from 'primevue/inputtext';
+import InputText from 'primevue/inputtext'
 
-const nroOt = ref(null);
+defineProps({
+  modelValue: { type: [String, Number, null], default: '' }
+})
 
-
-
+defineEmits(['update:modelValue'])
 </script>
 
 <style scoped>
+.otf-filter-element label {
+  white-space: nowrap;
+}
+
+.otf-filter-element :deep(.p-inputtext) {
+  width: 100%;
+}
 </style>
