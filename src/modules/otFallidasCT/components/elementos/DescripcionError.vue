c1d7a6359c99fa29649bd2c0cd8830c0ebebc512
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-03 13:39:10 -0300

    Add Descripcion Error PrimeVue filter element

---
+++ src/modules/otFallidasCT/components/elementos/DescripcionError.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="fm-field fm-field--span-4 otf-filter-element otf-filter-element--descripcion-error">
+    <label for="otf-descripcion-error">Descripcion Error</label>
+    <InputText
+      id="otf-descripcion-error"
+      :modelValue="modelValue"
+      class="w-full"
+      type="text"
+      autocomplete="off"
+      @update:modelValue="$emit('update:modelValue', $event)"
+    />
+  </div>
+</template>
+
+<script setup>
+import InputText from 'primevue/inputtext'
+
+defineProps({
+  modelValue: { type: [String, Number, null], default: '' }
+})
+
+defineEmits(['update:modelValue'])
+</script>
+
+<style scoped>
+.otf-filter-element label {
+  white-space: nowrap;
+}
+
+.otf-filter-element :deep(.p-inputtext) {
+  width: 100%;
+}
+</style>
