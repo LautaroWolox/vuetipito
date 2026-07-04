803eb6c21d9266a08efb195f3f51b6b4cf1b4498
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-04 02:50:25 -0300

    Use agreed PrimeVue grid action icons

--- src/components/shared/FmGridActions.vue
+++ src/components/shared/FmGridActions.vue
@@ -2,10 +2,10 @@
   <div class="fm-grid-actions fm-ui-grid-actions">
     <Button
       v-if="showExport"
-      icon="pi pi-file-export"
+      icon="pi pi-download"
       text
       rounded
-      class="fm-icon-btn fm-ui-icon-button"
+      class="fm-icon-btn fm-ui-icon-button fm-grid-action-button"
       title="Exportar"
       aria-label="Exportar"
       @click="$emit('export')"
@@ -16,7 +16,7 @@
       icon="pi pi-trash"
       text
       rounded
-      class="fm-icon-btn fm-ui-icon-button"
+      class="fm-icon-btn fm-ui-icon-button fm-grid-action-button"
       title="Excluir seleccionados"
       aria-label="Excluir seleccionados"
       @click="$emit('delete')"
@@ -27,7 +27,7 @@
       icon="pi pi-refresh"
       text
       rounded
-      class="fm-icon-btn fm-ui-icon-button"
+      class="fm-icon-btn fm-ui-icon-button fm-grid-action-button"
       title="Reprocesar"
       aria-label="Reprocesar"
       @click="$emit('refresh')"
@@ -39,18 +39,9 @@
 import Button from 'primevue/button'
 
 defineProps({
-  showExport: {
-    type: Boolean,
-    default: true
-  },
-  showDelete: {
-    type: Boolean,
-    default: true
-  },
-  showRefresh: {
-    type: Boolean,
-    default: true
-  }
+  showExport: { type: Boolean, default: true },
+  showDelete: { type: Boolean, default: true },
+  showRefresh: { type: Boolean, default: true }
 })
 
 defineEmits(['export', 'delete', 'refresh'])
