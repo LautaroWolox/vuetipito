ea5914403dc5976ef80db9af5229236fb1d6cb1d
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-05 07:42:57 -0300

    Add jobtype contrato columns

---
+++ src/modules/parametrizaciones/jobtypeContrato/columns.js
@@ -0,0 +1,51 @@
+export const jobtypeContratoColumns = [
+  {
+    field: 'codigoTarea',
+    header: 'CODIGO_TAREA',
+    width: '160px',
+    minWidth: '130px',
+    exportable: true
+  },
+  {
+    field: 'tarea',
+    header: 'TAREA',
+    width: '360px',
+    minWidth: '260px',
+    exportable: true
+  },
+  {
+    field: 'nombreContrato',
+    header: 'NOMBRE_CONTRATO',
+    width: '220px',
+    minWidth: '170px',
+    exportable: true
+  },
+  {
+    field: 'usuarioModificacion',
+    header: 'USUARIO_MODIFICACION',
+    width: '210px',
+    minWidth: '170px',
+    exportable: true
+  },
+  {
+    field: 'fechaModificacion',
+    header: 'FECHA_MODIFICACION',
+    width: '210px',
+    minWidth: '170px',
+    exportable: true
+  },
+  {
+    field: 'activo',
+    header: 'ACTIVO',
+    width: '140px',
+    minWidth: '110px',
+    exportable: true
+  },
+  {
+    field: 'pais',
+    header: 'PAIS',
+    width: '150px',
+    minWidth: '120px',
+    exportable: true
+  }
+]
