309f1afec07d91648c10afa7b759b9b97fa5fdc5
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-05 23:49:27 -0300

    Importar estilo grilla popup jobtype

--- src/main.js
+++ src/main.js
@@ -24,6 +24,7 @@ import './assets/css/modules/parametrizaciones/jobtype-popup-final-fix.css';
 import './assets/css/modules/parametrizaciones/jobtype-close-kill.css';
 import './assets/css/modules/parametrizaciones/jobtype-trash-icon-fix.css';
 import './assets/css/modules/parametrizaciones/jobtype-grid-icon-alignment.css';
+import './assets/css/modules/parametrizaciones/jobtype-popup-grid-main-like.css';
 import Lara from '@primeuix/themes/lara';
 
 import { definePreset } from '@primeuix/themes';
