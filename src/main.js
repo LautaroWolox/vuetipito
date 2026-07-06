93578a3bc65844294f5d896c20c7b9410f10cc87
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-05 23:23:12 -0300

    Importar fix tachito jobtype

--- src/main.js
+++ src/main.js
@@ -22,6 +22,7 @@ import './assets/css/modules/parametrizaciones/jobtype-contrato-compact.css';
 import './assets/css/modules/parametrizaciones/jobtype-popup-layout.css';
 import './assets/css/modules/parametrizaciones/jobtype-popup-final-fix.css';
 import './assets/css/modules/parametrizaciones/jobtype-close-kill.css';
+import './assets/css/modules/parametrizaciones/jobtype-trash-icon-fix.css';
 import Lara from '@primeuix/themes/lara';
 
 import { definePreset } from '@primeuix/themes';
