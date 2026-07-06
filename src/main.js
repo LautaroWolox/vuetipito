09bf8c8be349776a19d335580b1a63865f687930
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-06 00:20:11 -0300

    Importar fix sobrante grilla popup jobtype

--- src/main.js
+++ src/main.js
@@ -25,6 +25,7 @@ import './assets/css/modules/parametrizaciones/jobtype-close-kill.css';
 import './assets/css/modules/parametrizaciones/jobtype-trash-icon-fix.css';
 import './assets/css/modules/parametrizaciones/jobtype-grid-icon-alignment.css';
 import './assets/css/modules/parametrizaciones/jobtype-popup-grid-main-like.css';
+import './assets/css/modules/parametrizaciones/jobtype-popup-grid-gutter-fix.css';
 import Lara from '@primeuix/themes/lara';
 
 import { definePreset } from '@primeuix/themes';
