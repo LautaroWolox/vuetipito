2a52c7601944a58a23998b9e892f8e8b2e3d6fe6
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-06 21:13:35 -0300

    Configurar bloqueo y ancho de filtros OT

--- src/modules/otFallidasCT/components/Filtros.vue
+++ src/modules/otFallidasCT/components/Filtros.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="fm-panel-content fm-panel-content--accent fm-filters otf-filters">
     <div class="fm-filter-grid otf-filter-grid">
-      <NroOT v-model="store.filters.nroOt" />
-      <FechaDesde v-model="store.filters.fechaDesde" />
-      <FechaHasta v-model="store.filters.fechaHasta" />
-      <Contratista v-model="store.filters.contratista" :options="store.contratistas" />
-      <DescripcionError v-model="store.filters.descripcionError" />
-      <Excluida v-model="store.filters.excluida" :options="store.excluidas" />
-      <Pais v-model="store.filters.pais" :options="store.paises" />
+      <NroOT v-model="store.filters.nroOt" :disabled="nroOtFilterDisabled" />
+      <FechaDesde v-model="store.filters.fechaDesde" :disabled="otherFiltersDisabled" />
+      <FechaHasta v-model="store.filters.fechaHasta" :disabled="otherFiltersDisabled" />
+      <Contratista v-model="store.filters.contratista" :options="store.contratistas" :disabled="otherFiltersDisabled" />
+      <DescripcionError v-model="store.filters.descripcionError" :disabled="otherFiltersDisabled" />
+      <Excluida v-model="store.filters.excluida" :options="store.excluidas" :disabled="otherFiltersDisabled" />
+      <Pais v-model="store.filters.pais" :options="store.paises" :disabled="otherFiltersDisabled" />
     </div>
 
     <div class="fm-actions fm-filter-actions otf-filter-actions">
@@ -18,6 +18,7 @@
 </template>
 
 <script setup>
+import { computed } from 'vue'
 import { useFallidasCtStore } from '../store/CtFallidaStore'
 import NroOT from './elementos/NroOT.vue'
 import FechaDesde from './elementos/FechaDesde.vue'
@@ -28,6 +29,8 @@ import Excluida from './elementos/Excluida.vue'
 import Pais from './elementos/Pais.vue'
 
 const store = useFallidasCtStore()
+const nroOtFilterDisabled = computed(() => Boolean(store.disableNroOtFilter))
+const otherFiltersDisabled = computed(() => Boolean(store.disableOtherFilters))
 const limpiar = () => store.clearFilters()
 const buscar = async () => store.search()
 </script>
@@ -35,6 +38,9 @@ const buscar = async () => store.search()
 <style scoped>
 .otf-filters {
   width: 100%;
+  border-left-width: 1px !important;
+  border-left-style: solid !important;
+  border-left-color: #00a9bd !important;
 }
 
 .otf-filter-grid {
