1ee238eaa082b2146a608da19b0fa638c2c4b6c7
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-08 00:35:12 -0300

    feat: add cmo actividad columns

---
+++ src/modules/parametrizaciones/configuraCmoActividad/columns.js
@@ -0,0 +1,51 @@
+export const cmoActividadColumns = [
+  {
+    field: 'codigoActividad',
+    header: 'CODIGO_ACTIVIDAD',
+    width: '180px',
+    minWidth: '150px',
+    exportable: true
+  },
+  {
+    field: 'descActividad',
+    header: 'DESC_ACTIVIDAD',
+    width: '220px',
+    minWidth: '180px',
+    exportable: true
+  },
+  {
+    field: 'codigoS4',
+    header: 'CODIGO_S4',
+    width: '180px',
+    minWidth: '150px',
+    exportable: true
+  },
+  {
+    field: 'cmo',
+    header: 'CMO',
+    width: '180px',
+    minWidth: '150px',
+    exportable: true
+  },
+  {
+    field: 'usuarioModificacion',
+    header: 'USUARIO_MODIFICACION',
+    width: '220px',
+    minWidth: '180px',
+    exportable: true
+  },
+  {
+    field: 'fechaModificacion',
+    header: 'FECHA_MODIFICACION',
+    width: '220px',
+    minWidth: '180px',
+    exportable: true
+  },
+  {
+    field: 'activo',
+    header: 'ACTIVO',
+    width: '160px',
+    minWidth: '130px',
+    exportable: true
+  }
+]
